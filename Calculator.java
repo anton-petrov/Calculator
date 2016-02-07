51f15fd503955caaae5963d3f69c1756e952afdf
Anton Petrov <petrov.anton@live.com>
2016-02-07 18:25:17 +0300

    Calculator.java changed.

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,12 @@
 public class Calculator {
-  
+
+  private double result;
+
+  public Calculator() {
+    // constructor
+  }
+
+  public Calculator(double result) {
+    this.result = result;
+  }
 }
